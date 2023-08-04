fc9869735be8e7bf89c52866a08def039fdbe535
Mike Alexandre <mikealexadre484@gmail.com>
2023-08-04 11:32:02 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("rodando o sistema de gerenciamento de jogos")
 console.log ("aplicação se conectando ao banco de dados")
 // mais um teste de log 
-console.log("mais um teste")
+console.log("mais um teste")    
+console.log("enviando para o github")
